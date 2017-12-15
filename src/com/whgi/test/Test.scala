ecb42e58dbab469402cdb9a464040f3ced960bef
linlei <247147434@qq.com>
2017-12-15 16:09:52 +0800

    ScalaTest

--- src/com/whgi/test/Test.scala
+++ src/com/whgi/test/Test.scala
@@ -3,7 +3,7 @@ package com.whgi.test
 object Test {
   def main(args: Array[String]): Unit = {
     var a=10;
-     val re= if (a>6) "11" else String
+     val re= if (a>7) "11" else String
     println(re)
   }
 }
